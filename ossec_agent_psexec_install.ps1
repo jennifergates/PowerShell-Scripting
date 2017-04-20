3f45c16e0ec753689862506497bf82bbca067e9a
jennifergates <jennifergates@users.noreply.github.com>
2017-04-20 22:57:46 +0200

    initial upload - script incomplete

---
+++ ossec_agent_psexec_install.ps1
@@ -0,0 +1,88 @@
+﻿<#
+    .Synopsis
+        This script will install the ossec agent on a remote system using psexec to run commands on the system
+    .Description
+        This script takes an input file that lists the windows computers by IP and hostname. It also requires the 
+		path to client.keys files for each computer on the list. It also 
+		
+		The script installs the Windows OSSEC agent silently, copies the host's <hostname>_client.keys and ossec.conf 
+		files to the program's folder, and then starts the ossec agent service.
+		
+    .Example
+        ./ossec_agent_psexec_install.ps1 -computers_file mycomputers.txt -keys C:\ossec_keys\ -exe ossec-agent-win32-2.8.3.exe 
+		-config ossec.config
+
+    .Parameter computers_file
+        file with list of computer IPs and hostnames to install the agent onto
+	.Parameter keys
+        path to the <hostname>_client.keys files
+	.Parameter exe
+        ossec agent installer file
+	.Parameter config
+        path to the ossec config file
+    .Notes
+        NAME: ./ossec_agent_psexec_install.ps1
+        AUTHOR: Jennifer Gates
+        VERSION: 1.00
+        LASTEDIT: 20 April 2017
+        CHANGELOG:
+            1.00 - initial script 
+    
+        Output Colors:
+        White - Input Required
+        Cyan - Informational
+        Yellow - Warning
+        Red - Error
+
+#>
+#-------------------------------- Parameters --------------------------------#
+
+Param(
+	[Parameter(Mandatory=$True)]
+	[string] $computers_file,
+	
+	[Parameter(Mandatory=$True)]
+	[string] $keys,
+	
+	[Parameter(Mandatory=$True)]
+	[string] $exe,
+	
+	[Parameter(Mandatory=$True)]
+	[string] $config
+)
+
+
+#-------------------------------- Variables --------------------------------#
+
+
+$cred = get-credential
+$pass = $cred.getnetworkcredential().password
+$user = $cred.username
+$output = "ossec_installs.txt"
+
+#Check if input file exists. Exit if not.
+$FileExists = Test-Path $computers_file
+if($FileExists -eq $False) {
+	write-host "$computers_file does not exist." 
+	exit
+} 
+
+#import computer hostname,IP from input file	
+$computers = (Get-Content $computers_file)
+#write-host $computers[0]
+
+# loop through each computer and install ossec agent, copy files, start service
+foreach ($remote in $computers)
+{
+	$remote = $remote.split(",")
+	$hostname = $remote[0]
+	$ip = $remote[1]
+	$keyfile = $hostname + "_client.keys"
+	write-host $keyfile
+	"Installing on $hostname"#| out-file $output -append
+	"__________________________________________________________________________" >> $output
+    & C:\Users\Administrator\Desktop\SysInternals\PsExec.exe \\$ip -u $user -p $pass -c ossec-agent-win32-2.8.3.exe /S -accepteula >> $output
+	
+	#need to finish command - copy keys file to correct location.
+	& C:\Users\Administrator\Desktop\SysInternals\PsExec.exe \\$ip -u $user -p $pass cmd /c copy -accepteula >> $output
+}
